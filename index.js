b648da0f40084ac91ea907470ccabded507bdbb2
Katerina <92430746+ChrysostomouNygren@users.noreply.github.com>
2022-08-15 13:42:03 +0200

    fixing error?

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const knex = require("./db");
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/", "https://listiluring.herokuapp.com/"],
-    credentials: true
+    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/", "https://listiluring.herokuapp.com/"]
   },
 });
 
